228e80ea1ed19bee103b0fb54bf49f6afef16ddd
eldar-05 <eldarzh123@gmail.com>
2025-09-16 12:06:35 +0600

    adding user-friendly interface in console

--- MainMenu.java
+++ MainMenu.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
 public class MainMenu {
+
+    private static final String GREEN = "\u001B[32m";
+    private static final String RED = "\u001B[31m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String CYAN = "\u001B[36m";
+    private static final String RESET = "\u001B[0m";
+
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("\n=== Главное меню ===");
-            System.out.println("1. Hello World");
-            System.out.println("2. Калькулятор (заглушка)");
-            System.out.println("3. Генератор паролей");
-            System.out.println("4. Найти аккаунты через юзернеймы (Сталкеринг)");
-            System.out.println("0. Выход");
+            System.out.println("1. " + CYAN + "Hello World" + RESET);
+            System.out.println("2. " + CYAN + "Калькулятор (заглушка)" + RESET);
+            System.out.println("3. " + CYAN + "Генератор паролей" + RESET);
+            System.out.println("4. " + CYAN + "Найти аккаунты через юзернеймы (Сталкеринг)" + RESET);
+            System.out.println("0. " + RED + "Выход" + RESET);
             System.out.print("Выберите пункт: ");
             try {
                 int choice = (int)(scanner.nextInt());
@@ -30,10 +38,10 @@ public class MainMenu {
                     scanner.close();
                     return;
                 default:
-                    System.out.println("Неверный выбор!");
+                    System.out.println(RED + "Неверный выбор!" + RESET);
             } 
             } catch (Exception e) {
-                System.out.println("Ошибка ввода. Пожалуйста, введите число.");
+                System.out.println( RED + "Ошибка ввода. Пожалуйста, введите число." + RESET);
                 scanner.next();
                 continue;
             }
