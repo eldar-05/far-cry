c3b230c3fdd1658f58a0bbef1cb2971bfc7b9d44
nazarmatovbaiymbet <iamnazarmatov@gmail.com>
2025-09-19 17:31:30 +0600

    added Note

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-    public static void run() {
-        System.out.println("Hello, World!"); // пока что пример 
-    }
-}
---
+++ Note.java
@@ -0,0 +1,150 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class Note extends JFrame implements ActionListener {
+    private JTextArea textArea;
+    private JPanel topPanel;
+    private ArrayList<Document> documents = new ArrayList<>();
+    private Document currentDocument;
+    public Note() {
+        textArea = new JTextArea();
+        topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        
+        currentDocument = new Document("Untitled");
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JTextField titleOfDocumentTextField = new JTextField(currentDocument.getTitle());
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        fileMenu.add(openItem);
+        openItem.addActionListener(this);
+        fileMenu.add(saveItem);
+        saveItem.addActionListener(this);
+        fileMenu.add(exitItem);
+        exitItem.addActionListener(this);
+        menuBar.add(fileMenu);
+        menuBar.add(new JLabel("Title: "));
+        menuBar.add(titleOfDocumentTextField, BorderLayout.EAST);
+        setJMenuBar(menuBar);
+        
+        currentDocument.setContent(textArea.getText());
+                
+        setTitle("Note");
+        setSize(800, 1100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "Open":
+                JFrame openDocumentsFrame = new JFrame("Open Document");
+                openDocumentsFrame.setSize(400, 300);
+                JList <Document> documentList = new JList<>(documents.toArray(new Document[0]));
+                documentList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                documentList.addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(MouseEvent evt) {
+                        if (evt.getClickCount() == 2) {
+                            int index = documentList.locationToIndex(evt.getPoint());
+                            if (index >= 0) {
+                                currentDocument = documents.get(index);
+                                textArea.setText(currentDocument.getContent());
+                                openDocumentsFrame.dispose();
+                            }
+                        }
+                    }
+                });
+                JScrollPane scrollPane = new JScrollPane(documentList);
+
+                openDocumentsFrame.add(scrollPane);
+                openDocumentsFrame.setLocationRelativeTo(this);
+                openDocumentsFrame.setVisible(true);
+                break;
+            case "Save":
+                String title = currentDocument.getTitle();
+                if (!title.isEmpty()) {
+                    currentDocument.setTitle(title);
+                    currentDocument.setContent(textArea.getText());
+                    currentDocument.save();
+                    documents.add(currentDocument);
+                    JOptionPane.showMessageDialog(this, "Document saved as " + title, "Info", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "File name cannot be empty.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+
+            case "Exit":
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
+    }
+    public boolean checkExistingDocument() {
+        for (Document doc : documents){
+            if (currentDocument.getTitle().equals(doc.getTitle())) {
+                currentDocument = doc;
+                textArea.setText(currentDocument.getContent());
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return false;
+    }
+    public static void run() {
+        new Note();
+    }
+}
+
+class Document {
+    private String content;
+    private String title;
+    private boolean isModified;
+
+    public Document(String title) {
+        this.title = title;
+        this.content = "";
+        this.isModified = false;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        this.isModified = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isModified() {
+        return isModified;
+    }
+
+    public void save() {
+        isModified = false;
+    }
+
+    @Override
+    public String toString() {
+        return title + (isModified ? " *" : "");
+    }
+}
